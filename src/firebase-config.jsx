c2cb0c247ccaf8a37a051aa382a5c6f7c17dcc42
Ayush Mishra <ayushbm84@gmail.com>
2025-07-28 11:29:54 +0530

    getting started with project

---
+++ src/firebase-config.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZDPyFjxZo02dQUPocIcqjd4yaUPSsj6Y",
+  authDomain: "writeon-bd926.firebaseapp.com",
+  projectId: "writeon-bd926",
+  storageBucket: "writeon-bd926.firebasestorage.app",
+  messagingSenderId: "117698739426",
+  appId: "1:117698739426:web:36f57f2f47826c032532bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
